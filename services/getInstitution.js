1c78f14b77fd156022e901abce6d2b974f2e3c4b
jakub-siwiec <jakub.siwiec@svcais.com>
2021-10-14 13:42:27 +0100

    Institution error handling

--- services/getInstitution.js
+++ services/getInstitution.js
@@ -1,3 +1,5 @@
+const OperationalCustomError = require('../libs/errors/OperationalCustomError')
+
 const getInstitutionById = require('../subscribers/plaidEndpoints/institution')
 const getItem = require('./getItem')
 
@@ -5,7 +7,7 @@ const getItem = require('./getItem')
 const getInstitution = async (accessToken) => {
     const item = await getItem(accessToken)
     if (!item.item || !item.item.institution_id) {
-        return null
+        throw new OperationalCustomError(404, 'InstitutionNotFound', 'ERR_NOT_FOUND', 'NOT_FOUND', 'Institution not found in the Plaid Item')
     }
     const institution = await getInstitutionById(item.item.institution_id)
     return institution
